ec60ee5cacf62d5304bbc602f394e37c7a2f46c6
PRANAV77-PY <pbse1377@gmail.com>
2025-09-20 17:06:24 +0530

    Add files via upload

---
+++ example2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+pd.options.display.max_rows = 999
+
+df = pd.read_csv('D:\Software engg Professional training\Python Basics\Python Pandas\pandas Read CSV\data.csv')
+
+print(df)
